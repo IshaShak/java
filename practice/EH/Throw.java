dbf1e463535c9175b68165691f3d48150f1cd16e
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-08-28 11:37:51 +0530

    Add age validation with exception handling

---
+++ practice/EH/Throw.java
@@ -0,0 +1,22 @@
+package eh;
+
+public class ThrowEx {
+
+	    static void checkAge(int age) {
+	        if (age < 18) {
+	            throw new ArithmeticException("Age must be 18 or above");
+	        } else {
+	            System.out.println("You are eligible");
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	    	 try {
+		            checkAge(19);
+		        }
+		        catch (ArithmeticException e) {
+		            System.out.println(e.getMessage());
+		        }
+		    }
+
+}
