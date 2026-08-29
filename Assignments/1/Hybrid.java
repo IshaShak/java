686f2dfbdd96cc322a9eaa7eefa5072ee4e0dc77
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-08-29 12:34:39 +0530

    Update Hybrid.java

--- Assignments/1/Hybrid.java
+++ Assignments/1/Hybrid.java
@@ -1,107 +1,37 @@
-/*
- * Hybrid inheritance is a combination of two or more
- * types of inheritance.
- *
- * Java does not support hybrid inheritance using
- * classes alone because of the ambiguity problem.
- *
- * We can achieve a hybrid structure using classes
- * together with interfaces.
- *
- *              Employee
- *                  |
- *               Manager
- *               /     \
- *              /       \
- *        Developer     Tester
- *           |             |
- *      Programmer      Testable
- *       interface       interface
- *
- * Developer:
- * - extends Manager
- * - implements Programmer
- *
- * Tester:
- * - extends Manager
- * - implements Testable
- */
-
-static class Employee {
-
-    void work() {
-        System.out.println("Employee is working.");
+// File: HybridInheritance.java
+class Animal {
+    void eat() {
+        System.out.println("Animal eats food.");
     }
 }
 
-static class Manager extends Employee {
-
-    void manage() {
-        System.out.println("Manager is managing the team.");
-    }
+interface Flyable {
+    void fly();
 }
 
-interface Programmer {
-
-    void code();
-}
-
-interface Testable {
-
-    void test();
-}
-
-static class Developer extends Manager implements Programmer {
-
+// Bat extends Animal (Class Inheritance) AND implements Flyable (Interface Implementation)
+class Bat extends Animal implements Flyable {
     @Override
-    public void code() {
-        System.out.println("Developer is writing code.");
+    public void fly() {
+        System.out.println("Bat flies at night.");
+    }
+
+    void locateWithSound() {
+        System.out.println("Bat uses echolocation.");
     }
 }
 
-static class Tester extends Manager implements Testable {
-
-    @Override
-    public void test() {
-        System.out.println("Tester is testing the application.");
+public class HybridInheritance {
+    public static void main(String[] args) {
+        Bat bat = new Bat();
+        
+        // Inherited from Animal class
+        bat.eat();
+        
+        // Implemented from Flyable interface
+        bat.fly();
+        
+        // Method belonging to Bat
+        bat.locateWithSound();
     }
-}
-public static void main(String[] args) {
-
-Developer developer = new Developer();
-Tester tester = new Tester();
-
-/*
- * Developer inherits work() from Employee through Manager.
- */
-developer.work();
-
-/*
- * Developer inherits manage() from Manager.
- */
-developer.manage();
-
-/*
- * Developer implements Programmer,
- * so it must provide the code() method.
- */
-developer.code();
-
-System.out.println();
-
-/*
- * Tester inherits work() from Employee through Manager.
- */
-tester.work();
-
-/*
- * Tester inherits manage() from Manager.
- */
-tester.manage();
-
-/*
- * Tester implements Testable,
- * so it provides the test() method.
- */
-tester.test();
 }
