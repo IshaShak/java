8cb5db4798827cef7eafb6181332f22b043a884c
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-09-10 23:33:16 +0530

    Create Equals.java

---
+++ Assignments/3/Equals.java
@@ -0,0 +1,8 @@
+package strings;
+class StringEquals {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        System.out.println("Equal = " + s1.equals(s2));
+    }
+}
