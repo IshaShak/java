a0629ca82fc89baf944e9de60b0d83faafaae011
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-09-10 22:18:56 +0530

    Create NPE.java

---
+++ Observation/Exceptions/NPE.java
@@ -0,0 +1,14 @@
+package exceptions;
+class NullException {
+    public static void main(String[] args) {
+        String str = null;
+
+        try {
+            System.out.println("Finding string length...");
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e) {
+            System.out.println("Exception: String value is null");
+        }
+    }
+}
