21c07de22e3ca732924be40a019078fd364a08f6
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-09-10 23:41:53 +0530

    Create CharAt.java

---
+++ Assignments/3/CharAt.java
@@ -0,0 +1,7 @@
+package strings;
+class StringCharAt {
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println("Character = " + s.charAt(1));
+    }
+}
