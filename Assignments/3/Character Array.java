d1e6db5af416158a5e60fc1a8720279f08faf68b
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-09-10 23:47:27 +0530

    Create Character Array.java

---
+++ Assignments/3/Character Array.java
@@ -0,0 +1,8 @@
+package strings;
+class CharacterArrayString {
+    public static void main(String[] args) {
+        char[] ch = {'J', 'a', 'v', 'a'};
+        String str = new String(ch);
+        System.out.println(str);
+    }
+}
