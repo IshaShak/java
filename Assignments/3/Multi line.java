45c44aed5fd3c40fe0073c45b2a1cdee6699959c
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-09-10 23:50:30 +0530

    Create Multi line.java

---
+++ Assignments/3/Multi line.java
@@ -0,0 +1,12 @@
+package strings;
+class MultilineString {
+    public static void main(String[] args) {
+        String str = """
+                Hello
+                Welcome
+                To Java
+                """;
+
+        System.out.println(str);
+    }
+}
