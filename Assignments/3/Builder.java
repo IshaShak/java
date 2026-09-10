d410adc5f824b5bd41eec4f40a1c3369de66bd70
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-09-10 23:43:20 +0530

    Create Builder.java

---
+++ Assignments/3/Builder.java
@@ -0,0 +1,8 @@
+package strings;
+class StringBuilderExample {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("Hello");
+        str.append(" Java");
+        System.out.println(str);
+    }
+}
