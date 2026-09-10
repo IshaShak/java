5a77cfb9711d3a4505fd8d5f7ae85d079212771a
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-09-10 23:46:24 +0530

    Create With Numbers.java

---
+++ Assignments/3/With Numbers.java
@@ -0,0 +1,9 @@
+package strings;
+class StringWithNumbers {
+    public static void main(String[] args) {
+        int age = 15;
+        String str = "My age is " + age;
+
+        System.out.println(str);
+    }
+}
