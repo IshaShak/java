a42840a5824e59fe4621e26ca0f25d57811a712e
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-09-10 23:32:20 +0530

    Create S Length.java

---
+++ Assignments/3/S Length.java
@@ -0,0 +1,7 @@
+package strings;
+class StringLower {
+    public static void main(String[] args) {
+        String s = "HELLO";
+        System.out.println("Lowercase = " + s.toLowerCase());
+    }
+}
