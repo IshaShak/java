0e6149e21a24f7d0497f5cce7de544ff308b6720
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-09-10 23:37:54 +0530

    Create Concatenate.java

---
+++ Assignments/3/Concatenate.java
@@ -0,0 +1,8 @@
+package strings;
+class StringConcat {
+    public static void main(String[] args) {
+        String s1 = "Hi";
+        String s2 = "Guyss";
+        System.out.println("Result = " + s1.concat(s2));
+    }
+}
