78fc19ec415fa41ab9a2542045010159a2960eca
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-09-10 23:24:57 +0530

    Create code.java

---
+++ practice/Interface/code.java
@@ -0,0 +1,21 @@
+package interface;
+interface Vehicle{
+    void start();
+    void stop();
+}
+class Car implements Vehicle{    
+    public void start(){
+        System.out.println("Car strats");
+    }    
+    public void stop(){
+        System.out.println("car failed to start");
+    }
+}
+public class Interface {
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.start();
+        c.stop();
+    }
+    
+}
