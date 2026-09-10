ce59b5e51906b12c95729f041471457fbabf9ee7
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-09-10 23:39:52 +0530

    Create Upper.java

---
+++ Assignments/3/Upper.java
@@ -0,0 +1,7 @@
+package strings;
+class StringUpper {
+    public static void main(String[] args) {
+        String s = "hello";
+        System.out.println("Uppercase = " + s.toUpperCase());
+    }
+}
