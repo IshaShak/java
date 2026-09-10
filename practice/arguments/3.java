0ca4e8df95be3dbcec60686e2a5c119ae1dd10e5
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-09-10 22:57:57 +0530

    Create 3.java

---
+++ practice/arguments/3.java
@@ -0,0 +1,11 @@
+package basics;
+// Method without parameters and without return value
+public class Methods {  
+    void parameters() {
+        System.out.println("chiru");
+    }
+    public static void main(String[] args) {
+        Methods pr = new Methods();
+        pr.parameters();
+    }
+}
