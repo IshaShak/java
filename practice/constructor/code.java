9d0ee80caf4019692dd196d71a3ad8356496dca9
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-09-10 23:05:46 +0530

    Update code.java

--- practice/constructor/code.java
+++ practice/constructor/code.java
@@ -1,16 +1,13 @@
 package javacore;
-
 public class ConDemo {
-// class or instance variables
 	int rollNo;
     String name;
     float fee;
-	
 	// constructor 
 	public ConDemo() { 
-	 this.rollNo = 240;
-	 this.name= "chiru";
-	 this.due= 60999F;
+	 this.rollNo = 52;
+	 this.name= "sufiyy";
+	 this.due= 609F;
 	}
 	void sample(){
 		System.out.println("ROLL NO is:" + this.rollNo);
@@ -19,14 +16,11 @@ public class ConDemo {
 	}
 	void example() {
 		System.out.println("some other method named example");
-		
 	}
-	
 	public static void main(String[] args) {
 		System.out.println("This is main method..!");
 		ConDemo cd = new ConDemo();
 		cd.sample();
 		cd.example();
 	}
-		
-	}
+}
