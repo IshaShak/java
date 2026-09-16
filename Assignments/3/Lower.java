a573a1ca1a41963bee8cd4095b7e382f919e7069
IshaShak <shaikafsheenisha.25.csm@anits.edu.in>
2026-09-16 21:58:29 +0530

    Update Lower.java

--- Assignments/3/Lower.java
+++ Assignments/3/Lower.java
@@ -1,4 +1,5 @@
-class LowercaseString {
+package strings;
+public class LowercaseString {
     public static void main(String[] args) {
         String str = "HELLO JAVA"; System.out.println(str.toLowerCase());
     }
